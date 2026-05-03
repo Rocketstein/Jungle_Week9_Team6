5210f88c46db3e031a0d89f8d436a219d0487be5
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-03 22:49:46 +0900

    Feat: Add AmustJump/SlideObstacleActor baseplate

---
+++ KraftonEngine/Source/Game/GameActors/Obstacle/MustJumpObstacleActor.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "ObstacleActorBase.h"
+
+class AMustJumpObstacleActor : public AObstacleActorBase {
+public:
+	DECLARE_CLASS(AMustJumpObstacleActor, AObstacleActorBase)
+	void InitDefaultComponents(const FString& UStaticMeshFileName) override;
+	void OnPlayerCollision() override {}
+
+private:
+
+
+};
